7dd1388ae570f06e98f341c8c173562bbb745aed
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-01 20:37:12 +0200

    Added first commit with the initial project

---
+++ Prodam/Prodam/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Prodam
+//
+//  Created by Ramon Gilabert Llop on 5/1/15.
+//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
