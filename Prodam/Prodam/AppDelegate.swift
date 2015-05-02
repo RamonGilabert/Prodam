23ecda8e10a9d66e1a7674b742e50007d5bdd99a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-02 21:35:33 +0200

    Added some files

--- Prodam/Prodam/AppDelegate.swift
+++ Prodam/Prodam/AppDelegate.swift
@@ -3,17 +3,18 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    var mainWindowController: MainWindowController?
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let window = NSWindow(contentRect: NSMakeRect(100, 100, 200, 200), styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, defer: false)
+        window.title = "New Window"
+        window.opaque = false
+        window.center()
+        window.movableByWindowBackground = true
+        window.makeKeyAndOrderFront(true)
+
+        self.mainWindowController = MainWindowController(window: window)
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
 
