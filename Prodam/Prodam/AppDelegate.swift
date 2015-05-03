3884d4afc84a7ffdb0b7e09d96d247988b507bcf
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-03 10:21:55 +0200

    Added the code for the popover!

--- Prodam/Prodam/AppDelegate.swift
+++ Prodam/Prodam/AppDelegate.swift
@@ -3,14 +3,10 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
-    var mainWindowController: MainWindowController?
+    var mainPopoverController: PopoverController?
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        let iconMenu = NSImage(named: "menu-logo")
-        iconMenu?.setTemplate(true)
-
-        self.statusItem.image = iconMenu
+        self.mainPopoverController = PopoverController()
     }
 }
 
