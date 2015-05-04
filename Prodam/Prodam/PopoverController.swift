bfa2dbbea7a705f46bae5dbd9c029b42342f674b
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-04 09:52:22 +0200

    Playing with the button layout

--- Prodam/Prodam/PopoverController.swift
+++ Prodam/Prodam/PopoverController.swift
@@ -22,6 +22,15 @@ class PopoverController: NSViewController, NSPopoverDelegate {
         self.popover.delegate = self
 
         self.addTaskButton.frame = NSMakeRect(Constant.MainWindowLayout.MinimumPaddingButton, Constant.MainWindowLayout.MinimumPaddingButton, Constant.Window.Width - (2 * Constant.MainWindowLayout.MinimumPaddingButton), Constant.MainWindowLayout.HeightOfButton)
+        let buttonCell = self.addTaskButton.cell() as! NSButtonCell
+        buttonCell.backgroundColor = NSColor(calibratedHue:0.36, saturation:0.49, brightness:0.76, alpha:1)
+        let mutableStringAddTaskButton = NSMutableAttributedString(string: "START WORKING")
+        mutableStringAddTaskButton.addAttribute(NSForegroundColorAttributeName, value: NSColor.whiteColor(), range: NSMakeRange(0, mutableStringAddTaskButton.length))
+        mutableStringAddTaskButton.addAttribute(NSFontAttributeName, value: NSFont(name: "AvenirNext-DemiBold", size: 20)!, range: NSMakeRange(0, mutableStringAddTaskButton.length))
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.CenterTextAlignment
+        mutableStringAddTaskButton.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSMakeRange(0, mutableStringAddTaskButton.length))
+        self.addTaskButton.attributedTitle = mutableStringAddTaskButton
     }
 
     // MARK: Helper methods
--- Prodam/Prodam/ViewModel.swift
+++ Prodam/Prodam/ViewModel.swift
@@ -3,12 +3,12 @@ import Cocoa
 struct Constant {
     struct Window {
         static let Width = 300 as CGFloat
-        static let Height = 225 as CGFloat
+        static let Height = 200 as CGFloat
     }
 
     struct MainWindowLayout {
-        static let MinimumPaddingButton = 9 as CGFloat
-        static let HeightOfButton = Constant.Window.Height / 4
+        static let MinimumPaddingButton = 6 as CGFloat
+        static let HeightOfButton = Constant.Window.Height / 3.5
     }
 }
 
