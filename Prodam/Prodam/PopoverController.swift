948c21a80d1981e979266ac7a804d0786ed1333a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-04 09:25:03 +0200

    Added some more properties

--- Prodam/Prodam/PopoverController.swift
+++ Prodam/Prodam/PopoverController.swift
@@ -3,17 +3,20 @@ import Cocoa
 class PopoverController: NSViewController, NSPopoverDelegate {
 
     let popover = NSPopover()
+    let addTaskButton = NSButton()
+    let doneTaskButton = NSButton()
+    let pauseTaskButton = NSButton()
+    let taskButton = NSButton()
+    let settingsButton = NSButton()
+    let taskTextField = NSTextField()
+    let timerTextField = NSTextField()
 
     override func loadView() {
         self.view = NSView()
-        self.view.frame = NSMakeRect(0, 0, 275, 400)
+        self.view.frame = NSMakeRect(0, 0, Constant.Window.Width, Constant.Window.Height)
         
         self.popover.contentViewController = self
         self.popover.appearance = NSAppearance(named: NSAppearanceNameVibrantLight)
         self.popover.delegate = self
-
-        let button = NSButton(frame: NSMakeRect(0, 0, 100, 100))
-        button.title = "Sup"
-        self.view.addSubview(button)
     }
 }
---
+++ Prodam/Prodam/ViewModel.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+struct Constant {
+    struct Window {
+        static let Width = 300 as CGFloat
+        static let Height = 225 as CGFloat
+    }
+
+    struct MainWindowLayout {
+        static let MinimumPaddingButton = 19 as CGFloat
+    }
+}
+
+class ViewModel: NSObject {
+
+}
