1282701d86dabcbd06bee1eb37235d8ef3095139
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-04 10:40:11 +0200

    Adding more and more buttons

--- Prodam/Prodam/PopoverController.swift
+++ Prodam/Prodam/PopoverController.swift
@@ -24,8 +24,7 @@ class PopoverController: NSViewController, NSPopoverDelegate {
         self.addTaskButton.frame = NSMakeRect(Constant.MainWindowLayout.MinimumPaddingButton, Constant.MainWindowLayout.MinimumPaddingButton/1.5, Constant.Window.Width - (2 * Constant.MainWindowLayout.MinimumPaddingButton), Constant.MainWindowLayout.HeightOfButton)
         self.addTaskButton.image = NSImage(named: "background-start-button")
         self.addTaskButton.bordered = false
-        self.addTaskButton.layer?.masksToBounds = true
-        self.addTaskButton.layer?.cornerRadius = 7.5
+        self.addTaskButton.setButtonType(NSButtonType.MomentaryChangeButton)
         let mutableStringAddTaskButton = NSMutableAttributedString(string: "START WORKING")
         mutableStringAddTaskButton.addAttribute(NSForegroundColorAttributeName, value: NSColor.whiteColor(), range: NSMakeRange(0, mutableStringAddTaskButton.length))
         mutableStringAddTaskButton.addAttribute(NSFontAttributeName, value: NSFont(name: "AvenirNext-DemiBold", size: 20)!, range: NSMakeRange(0, mutableStringAddTaskButton.length))
@@ -34,7 +33,19 @@ class PopoverController: NSViewController, NSPopoverDelegate {
         mutableStringAddTaskButton.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSMakeRange(0, mutableStringAddTaskButton.length))
         self.addTaskButton.attributedTitle = mutableStringAddTaskButton
 
-        //self.taskButton.frame
+        self.taskButton.frame = NSMakeRect(14, Constant.Window.Height - 26, 21, 13)
+        self.taskButton.image = NSImage(named: "todo-button")
+        self.taskButton.bordered = false
+
+        self.settingsButton.frame = NSMakeRect(Constant.Window.Width - 34, Constant.Window.Height - 30, 20, 20)
+        self.settingsButton.image = NSImage(named: "settings-button")
+        self.settingsButton.bordered = false
+
+        self.taskTextField.frame = NSMakeRect(0, 0, 0, 0)
+        self.taskTextField.bordered = false
+        self.taskTextField.alignment = NSTextAlignment.CenterTextAlignment
+        self.taskTextField.placeholderString = "Enter your new task..."
+        self.taskTextField.font = NSFont(name: "HelveticaNeue", size: 19)
     }
 
     // MARK: Helper methods
