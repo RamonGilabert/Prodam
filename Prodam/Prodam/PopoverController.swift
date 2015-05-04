d5958d6762039cbabc7c4aabb49a5844a79b5735
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-04 20:29:01 +0200

    Added some great things!

--- Prodam/Prodam/PopoverController.swift
+++ Prodam/Prodam/PopoverController.swift
@@ -37,6 +37,32 @@ class PopoverController: NSViewController, NSPopoverDelegate, NSTextViewDelegate
         self.addTaskButton.target = self
         self.addTaskButton.action = "onStartWorkingButtonPressed"
 
+        self.doneTaskButton.frame = NSMakeRect(Constant.MainWindowLayout.MinimumPaddingButton, Constant.MainWindowLayout.MinimumPaddingButton/1.5, Constant.Window.Width/2 - (1.5 * Constant.MainWindowLayout.MinimumPaddingButton), Constant.MainWindowLayout.HeightOfButton)
+        self.doneTaskButton.image = NSImage(named: "background-done-button")
+        self.doneTaskButton.bordered = false
+        self.doneTaskButton.setButtonType(NSButtonType.MomentaryChangeButton)
+        let mutableStringDoneButton = NSMutableAttributedString(string: "STOP")
+        mutableStringDoneButton.addAttribute(NSForegroundColorAttributeName, value: NSColor.whiteColor(), range: NSMakeRange(0, mutableStringDoneButton.length))
+        mutableStringDoneButton.addAttribute(NSFontAttributeName, value: NSFont(name: "AvenirNext-DemiBold", size: 20)!, range: NSMakeRange(0, mutableStringDoneButton.length))
+        let paragraphStyleDoneButton = NSMutableParagraphStyle()
+        paragraphStyleDoneButton.alignment = NSTextAlignment.CenterTextAlignment
+        mutableStringDoneButton.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyleDoneButton, range: NSMakeRange(0, mutableStringDoneButton.length))
+        self.doneTaskButton.attributedTitle = mutableStringDoneButton
+        self.doneTaskButton.alphaValue = 0.0
+
+        self.pauseTaskButton.frame = NSMakeRect(Constant.MainWindowLayout.MinimumPaddingButton/2 + Constant.Window.Width/2, Constant.MainWindowLayout.MinimumPaddingButton/1.5, Constant.Window.Width/2 - (1.5 * Constant.MainWindowLayout.MinimumPaddingButton), Constant.MainWindowLayout.HeightOfButton)
+        self.pauseTaskButton.image = NSImage(named: "background-pause-button")
+        self.pauseTaskButton.bordered = false
+        self.pauseTaskButton.setButtonType(NSButtonType.MomentaryChangeButton)
+        let mutableStringPauseButton = NSMutableAttributedString(string: "PAUSE")
+        mutableStringPauseButton.addAttribute(NSForegroundColorAttributeName, value: NSColor.whiteColor(), range: NSMakeRange(0, mutableStringPauseButton.length))
+        mutableStringPauseButton.addAttribute(NSFontAttributeName, value: NSFont(name: "AvenirNext-DemiBold", size: 20)!, range: NSMakeRange(0, mutableStringPauseButton.length))
+        let paragraphStylePauseButton = NSMutableParagraphStyle()
+        paragraphStylePauseButton.alignment = NSTextAlignment.CenterTextAlignment
+        mutableStringPauseButton.addAttribute(NSParagraphStyleAttributeName, value: paragraphStylePauseButton, range: NSMakeRange(0, mutableStringPauseButton.length))
+        self.pauseTaskButton.attributedTitle = mutableStringPauseButton
+        self.pauseTaskButton.alphaValue = 0.0
+
         self.taskButton.frame = NSMakeRect(14, Constant.Window.Height - 26, 21, 13)
         self.taskButton.image = NSImage(named: "todo-button")
         self.taskButton.bordered = false
@@ -104,7 +130,11 @@ class PopoverController: NSViewController, NSPopoverDelegate, NSTextViewDelegate
         self.editableTimerTextField.alphaValue = 0.0
         self.minutesLabel.alphaValue = 0.0
         self.timerTextField.alphaValue = 1.0
-        self.timerTextField.stringValue = self.editableTimerTextField.stringValue
+        self.timerTextField.stringValue = "\(self.editableTimerTextField.stringValue):00"
+
+        self.addTaskButton.alphaValue = 0.0
+        self.doneTaskButton.alphaValue = 1.0
+        self.pauseTaskButton.alphaValue = 1.0
 
         // TODO: Perform animations of the textFields.
         // TODO: Perform animation of the buttons, turning into two buttons.
@@ -129,9 +159,9 @@ class PopoverController: NSViewController, NSPopoverDelegate, NSTextViewDelegate
         self.view.addSubview(self.settingsButton)
         self.view.addSubview(self.timerTextField)
         self.view.addSubview(self.taskTextField)
-        self.view.addSubview(self.addTaskButton)
         self.view.addSubview(self.doneTaskButton)
         self.view.addSubview(self.pauseTaskButton)
+        self.view.addSubview(self.addTaskButton)
         self.view.addSubview(self.editableTimerTextField)
         self.view.addSubview(self.minutesLabel)
     }
