5a2738909b3f62bfecbd1d9329eefab80d95f340
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-05 12:57:44 +0200

    First refactor, damn

--- Prodam/Prodam/ViewModel.swift
+++ Prodam/Prodam/ViewModel.swift
@@ -14,4 +14,31 @@ struct Constant {
 
 class ViewModel: NSObject {
 
+    // MARK: Main View Controller: Layout
+
+    func layoutFunctionalButtonsMainView() {
+        let button = NSButton()
+
+        button.frame = NSMakeRect(Constant.MainWindowLayout.MinimumPaddingButton, Constant.MainWindowLayout.MinimumPaddingButton/1.5, Constant.Window.Width - (2 * Constant.MainWindowLayout.MinimumPaddingButton), Constant.MainWindowLayout.HeightOfButton)
+        button.image = NSImage(named: "background-start-button")
+        button.bordered = false
+        button.setButtonType(NSButtonType.MomentaryChangeButton)
+        button.attributedTitle = attributedStringForButtons("START WORKING")
+        button.target = self
+        button.action = "onStartWorkingButtonPressed"
+    }
+
+    // MARK: Main View Controller: Helper methods
+
+    func attributedStringForButtons(stringValue: String) -> NSMutableString {
+        let mutableString = NSMutableAttributedString(string: stringValue)
+        mutableString.addAttribute(NSForegroundColorAttributeName, value: NSColor.whiteColor(), range: NSMakeRange(0, mutableString.length))
+        mutableString.addAttribute(NSFontAttributeName, value: NSFont(name: "AvenirNext-DemiBold", size: 20)!, range: NSMakeRange(0, mutableStringAddTaskButton.length))
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.CenterTextAlignment
+        mutableString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSMakeRange(0, mutableString.length))
+
+        return mutableString
+    }
 }
