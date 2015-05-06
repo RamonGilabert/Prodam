63cf3be1855e0b0686b75ab178981a3e694a142e
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-06 20:23:43 +0200

    Showing a window already!

--- Prodam/Prodam/AppDelegate.swift
+++ Prodam/Prodam/AppDelegate.swift
@@ -4,9 +4,11 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var mainPopoverController: PopoverManager?
+    let breakWindowController = BreakWindowController()
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         self.mainPopoverController = PopoverManager()
+        self.breakWindowController.loadWindow()
     }
 }
 
--- Prodam/Prodam/BreakWindowController.swift
+++ Prodam/Prodam/BreakWindowController.swift
@@ -2,12 +2,12 @@ import Cocoa
 
 class BreakWindowController: NSWindowController {
 
-    let popoverController = BreakController()
-    var popoverManager: PopoverManager?
-    var popover = NSPopover()
-
     override func loadWindow() {
-        self.window = NSWindow(contentRect: NSMakeRect(0, 0, 0, 0), styleMask: 1, backing: NSBackingStoreType.Buffered, defer: false)
+        self.window = NSWindow(contentRect: NSMakeRect(0, 0, 1400, 800), styleMask: NSBorderlessWindowMask | NSTexturedBackgroundWindowMask, backing: NSBackingStoreType.Buffered, defer: false)
+        self.window?.backgroundColor = NSColor.redColor()
+        self.window?.center()
+        self.window?.makeKeyAndOrderFront(true)
+        self.window?.display()
     }
 
 }
