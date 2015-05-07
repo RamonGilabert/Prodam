312fbb45d89f0c873ebbacf156cba1c3d514f124
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-07 12:31:44 +0200

    Updating some stuff

--- Prodam/Prodam/TextAttributter.swift
+++ Prodam/Prodam/TextAttributter.swift
@@ -2,10 +2,10 @@ import Cocoa
 
 class TextAttributter: NSObject {
 
-    class func attributedStringForButtons(stringValue: String) -> NSAttributedString {
+    class func attributedStringForButtons(stringValue: String, font: String, color: NSColor) -> NSAttributedString {
         let mutableString = NSMutableAttributedString(string: stringValue)
-        mutableString.addAttribute(NSForegroundColorAttributeName, value: NSColor.whiteColor(), range: NSMakeRange(0, mutableString.length))
-        mutableString.addAttribute(NSFontAttributeName, value: NSFont(name: "AvenirNext-DemiBold", size: 20)!, range: NSMakeRange(0, mutableString.length))
+        mutableString.addAttribute(NSForegroundColorAttributeName, value: color, range: NSMakeRange(0, mutableString.length))
+        mutableString.addAttribute(NSFontAttributeName, value: NSFont(name: font, size: 20)!, range: NSMakeRange(0, mutableString.length))
 
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.alignment = NSTextAlignment.CenterTextAlignment
