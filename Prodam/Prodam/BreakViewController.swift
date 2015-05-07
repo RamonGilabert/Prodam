a72efcbea3456bba163068547a8b1331cb2983e4
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-07 10:58:14 +0200

    Added a bunch of new properties

--- Prodam/Prodam/BreakViewController.swift
+++ Prodam/Prodam/BreakViewController.swift
@@ -2,6 +2,16 @@ import Cocoa
 
 class BreakViewController: NSViewController {
 
+    var headerLabel = NSTextField()
+    var minutesLabel = NSTextField()
+    var editableTextField = NSTextField()
+    var realTextField = NSTextField()
+    var sentenceLabel = NSTextField()
+    var authorLabel = NSTextField()
+    var workAgainButton = NSButton()
+    var startBreakButton = NSButton()
+    var closeWindowButton = NSButton()
+
     override func loadView() {
         self.view = NSView()
         self.view.frame = NSMakeRect(0, 0, 800, 600)
