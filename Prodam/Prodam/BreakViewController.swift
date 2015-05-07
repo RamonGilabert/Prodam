4db20df86dcb8622b7329e32215e12f9738f4348
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-07 11:19:24 +0200

    Finished adding fonts

--- Prodam/Prodam/BreakViewController.swift
+++ Prodam/Prodam/BreakViewController.swift
@@ -15,14 +15,15 @@ class BreakViewController: NSViewController {
     override func loadView() {
         self.view = NSView()
         self.view.frame = NSMakeRect(0, 0, 800, 600)
-        
+
         self.headerLabel.frame = NSMakeRect(0, 0, 0, 0)
         self.headerLabel.bordered = false
         self.headerLabel.bezeled = false
         self.headerLabel.editable = false
         self.headerLabel.selectable = false
+        self.headerLabel.drawsBackground = false
         self.headerLabel.stringValue = "TAKING A BREAK"
-        self.headerLabel.font = NSFont(name: "", size: 50)
+        self.headerLabel.font = NSFont(name: "OpenSans-Semibold", size: 50)
         self.headerLabel.sizeToFit()
         self.headerLabel.appearance = NSAppearance(named: NSAppearanceNameVibrantLight)
         self.view.addSubview(self.headerLabel)
