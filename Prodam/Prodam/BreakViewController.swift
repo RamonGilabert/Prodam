55de19072c39441fcc6308ac93cf53ecc9dc19f4
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-07 10:37:07 +0200

    Added an NSViewController working now

--- Prodam/Prodam/BreakViewController.swift
+++ Prodam/Prodam/BreakViewController.swift
@@ -3,6 +3,11 @@ import Cocoa
 class BreakViewController: NSViewController {
 
     override func loadView() {
-
+        self.view = NSView()
+        self.view.frame = NSMakeRect(0, 0, 800, 600)
+        
+        let button = NSButton(frame: NSMakeRect(0, 0, 200, 200))
+        button.title = "Sup"
+        self.view.addSubview(button)
     }
 }
--- Prodam/Prodam/BreakWindowController.swift
+++ Prodam/Prodam/BreakWindowController.swift
@@ -2,6 +2,8 @@ import Cocoa
 
 class BreakWindowController: NSWindowController {
 
+    let breakViewController = BreakViewController()
+
     override func loadWindow() {
         self.window = BreakWindow(contentRect: NSMakeRect(0, 0, 800, 600), styleMask: NSBorderlessWindowMask, backing: NSBackingStoreType.Buffered, defer: false)
         self.window?.contentView = RoundedCornerView(frame: self.window!.frame)
@@ -10,5 +12,7 @@ class BreakWindowController: NSWindowController {
         self.window?.display()
         self.window?.becomeKeyWindow()
         self.window?.becomeMainWindow()
+
+        self.window?.contentView.addSubview(self.breakViewController.view)
     }
 }
