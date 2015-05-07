b8ee620b6cf504b36d3b696d75dcb7fff076bb38
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-07 11:09:45 +0200

    Added custom fonts

--- Prodam/Prodam/BreakViewController.swift
+++ Prodam/Prodam/BreakViewController.swift
@@ -16,8 +16,16 @@ class BreakViewController: NSViewController {
         self.view = NSView()
         self.view.frame = NSMakeRect(0, 0, 800, 600)
         
-        let button = NSButton(frame: NSMakeRect(0, 0, 200, 200))
-        button.title = "Sup"
-        self.view.addSubview(button)
+        self.headerLabel.frame = NSMakeRect(0, 0, 0, 0)
+        self.headerLabel.bordered = false
+        self.headerLabel.bezeled = false
+        self.headerLabel.editable = false
+        self.headerLabel.selectable = false
+        self.headerLabel.stringValue = "TAKING A BREAK"
+        self.headerLabel.font = NSFont(name: "", size: 50)
+        self.headerLabel.sizeToFit()
+        self.headerLabel.appearance = NSAppearance(named: NSAppearanceNameVibrantLight)
+        self.view.addSubview(self.headerLabel)
+
     }
 }
