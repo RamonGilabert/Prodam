72325ea0d2eaf9ff8ecbfc070a2f5defa26bc7f1
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-08 12:58:11 +0200

    Added some more quotes

--- Prodam/Prodam/Quotes.swift
+++ Prodam/Prodam/Quotes.swift
@@ -3,31 +3,31 @@ import Foundation
 struct Quotes {
     static let One: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
 
-    static let Two: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Two: [String: String] = ["quote": "There is beauty when something works and it works intuitively.", "author": "Jonathan Ive"]
 
-    static let Three: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Three: [String: String] = ["quote": "Designing and developing anything of consequence is incredibly challenging.", "author": "Jonathan Ive"]
 
-    static let Four: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Four: [String: String] = ["quote": "Let your joy be in your journey - not in some distant goal.", "author": "Tim Cook"]
 
-    static let Five: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Five: [String: String] = ["quote": "You can only do so many things great, and you should cast aside everything else.", "author": "Tim Cook"]
 
-    static let Six: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Six: [String: String] = ["quote": "Design is not just what it looks like and feels like. Design is how it works.", "author": "Steve Jobs"]
 
-    static let Seven: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Seven: [String: String] = ["quote": "I want to put a ding in the universe.", "author": "Steve Jobs"]
 
-    static let Eight: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Eight: [String: String] = ["quote": "Sometimes life hits you in the head with a brick. Don't lose faith.", "author": "Steve Jobs"]
 
-    static let Nine: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Nine: [String: String] = ["quote": "Everything’s impossible until somebody does it.", "author": "Bruce Wayne"]
 
-    static let Ten: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Ten: [String: String] = ["quote": "Creativity is the sudden cessation of stupidity.", "author": "Edwin Land"]
 
-    static let Eleven: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Eleven: [String: String] = ["quote": "Do what you love and the rest will come.", "author": "Dennis Crowley"]
 
-    static let Twelve: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Twelve: [String: String] = ["quote": "Learn early, learn often.", "author": "Drew Houston"]
 
-    static let Thirteen: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Thirteen: [String: String] = ["quote": "If you want to succeed, you’ve got to be okay to just lose control.", "author": "Alexis Ohanian"]
 
-    static let Fourteen: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Fourteen: [String: String] = ["quote": "Fail often so you can succeed sooner.", "author": "Tom Kelley"]
 
-    static let Fifteen: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+    static let Fifteen: [String: String] = ["quote": "Good is the enemy of great.", "author": "Jim Collins"]
 }
