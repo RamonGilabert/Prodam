9b138a00880a1e0b4e6a034ce077e6742193dadc
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-08 12:48:35 +0200

    Added new class with quotes

---
+++ Prodam/Prodam/Quotes.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct Quotes {
+    static let One: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Two: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Three: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Four: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Five: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Six: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Seven: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Eight: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Nine: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Ten: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Eleven: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Twelve: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Thirteen: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Fourteen: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+
+    static let Fifteen: [String: String] = ["quote": "Stay hungry, stay foolish...", "author": "Steve Jobs"]
+}
