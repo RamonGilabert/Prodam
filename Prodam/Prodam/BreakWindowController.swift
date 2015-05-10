9d321c98fdad6caa1c909ef40d79f7f79b3fa971
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-10 09:56:05 +0200

    Animation done

--- Prodam/Prodam/BreakWindowController.swift
+++ Prodam/Prodam/BreakWindowController.swift
@@ -8,10 +8,11 @@ class BreakWindowController: NSWindowController {
 
     override func loadWindow() {
         self.window = BreakWindow(contentRect: self.breakViewController.view.frame, styleMask: NSBorderlessWindowMask, backing: NSBackingStoreType.Buffered, defer: false)
-        self.window?.contentView = RoundedCornerView(frame: self.window!.frame)
+        self.window?.contentView = RoundedCornerView(frame: self.breakViewController.view.frame)
         self.window?.center()
-        self.window?.makeKeyAndOrderFront(true)
+        self.window?.animationBehavior = NSWindowAnimationBehavior.AlertPanel
         self.window?.display()
+        self.window?.makeKeyAndOrderFront(true)
 
         (self.window?.contentView as! RoundedCornerView).visualEffectView.addSubview(self.breakViewController.view)
     }
