3087d248f2e3a8b816fbac89313be2ee2ca6f05c
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-11 10:49:39 +0200

    Added that separator

--- Prodam/PreferencesViewController.swift
+++ Prodam/PreferencesViewController.swift
@@ -17,7 +17,7 @@ class PreferencesViewController: NSViewController {
         titlePreferences.font = NSFont(name: "Helvetica Neue", size: 18)
         titlePreferences.alignment = NSTextAlignment.CenterTextAlignment
         titlePreferences.sizeToFit()
-        titlePreferences.frame = NSMakeRect((self.view.frame.width - titlePreferences.frame.width)/2, self.view.frame.height - titlePreferences.frame.height - 10, titlePreferences.frame.width, titlePreferences.frame.height)
+        titlePreferences.frame = NSMakeRect((self.view.frame.width - titlePreferences.frame.width)/2, self.view.frame.height - titlePreferences.frame.height - 11, titlePreferences.frame.width, titlePreferences.frame.height)
         self.view.addSubview(titlePreferences)
 
         let buttonClosePreferences = NSButton(frame: NSMakeRect(12, self.view.frame.height - 28, 16, 16))
@@ -28,12 +28,18 @@ class PreferencesViewController: NSViewController {
         buttonClosePreferences.action = "onClosePreferencesButtonPressed"
         self.view.addSubview(buttonClosePreferences)
 
-        let buttonQuitLogin = NSButton(frame: NSMakeRect(0, 0, 0, 0))
+        let buttonQuitLogin = NSButton(frame: NSMakeRect(self.view.frame.width - 29, self.view.frame.height - 30, 17, 18))
         buttonQuitLogin.bordered = false
         buttonQuitLogin.setButtonType(NSButtonType.MomentaryChangeButton)
-        buttonQuitLogin.image = NSImage(named: "button-quit-preferences"))
+        buttonQuitLogin.image = NSImage(named: "button-quit-preferences")
+        buttonQuitLogin.target = self
+        buttonQuitLogin.action = "onQuitAppButtonPressed"
+        self.view.addSubview(buttonQuitLogin)
 
-        let topSeparator = NSView()
+        let topSeparator = NSView(frame: NSMakeRect(0, self.view.frame.height - 45, self.view.frame.width, 1))
+        topSeparator.wantsLayer = true
+        topSeparator.layer?.backgroundColor = NSColor.lightGrayColor().CGColor
+        self.view.addSubview(topSeparator)
 
         let labelGeneral = NSTextField()
 
