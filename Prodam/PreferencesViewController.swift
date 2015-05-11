3a1fe4d8e668c1e1bad56c721226e0cdea0fb1e7
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-05-11 10:12:36 +0200

    Added new stuff to the table, playing with preferences now

--- Prodam/PreferencesViewController.swift
+++ Prodam/PreferencesViewController.swift
@@ -2,14 +2,23 @@ import Cocoa
 
 class PreferencesViewController: NSViewController {
 
-    let label = NSTextField()
+    let viewModel = ViewModel()
 
     override func loadView() {
         self.view = NSView(frame: NSMakeRect(0, 0, 500, 360))
 
-        self.label.frame = NSMakeRect(0, 0, 200, 200)
-        self.label.stringValue = "SUUUP"
-
-        self.view.addSubview(self.label)
+        let titlePreferences = NSTextField()
+        let buttonClosePreferences = NSButton()
+        let buttonQuitLogin = NSButton()
+        let topSeparator = NSView()
+        let labelGeneral = NSTextField()
+        let labelLaunchLogin = NSTextField()
+        let labelSoundWhenDone = NSTextField()
+        let bottomSeparator = NSView()
+        let labelAbout = NSTextField()
+        let labelName = NSTextField()
+        let buttonTwitter = NSButton()
+        let buttonWebsite = NSButton()
+        let buttonEmail = NSButton()
     }
 }
